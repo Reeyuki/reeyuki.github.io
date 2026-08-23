d3a538ba0d1f7184d1b7e1cddbba48ec21c013bd
TheLp281 <efekantunc0@gmail.com>
2026-08-23 19:34:52 +0200

    update yukicord

--- mainscript.js
+++ mainscript.js
@@ -23,7 +23,7 @@ const slideshows = {
   voraxoid: { current: 0, interval: null, duration: 8000 },
   yukios: { current: 0, interval: null, duration: 8000 },
   putitback: { current: 0, interval: null, duration: 8000 },
-  liventcord: { current: 0, interval: null, duration: 8000 },
+  yukicord: { current: 0, interval: null, duration: 8000 },
   gnome: { current: 0, interval: null, duration: 8000 },
 };
 function getState(gameId) {
